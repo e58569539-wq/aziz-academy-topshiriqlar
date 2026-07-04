24ef55b4f9cdea63adffb5932bc09bce60b263c4
e58569539-wq <e58569539@gmail.com>
2026-07-03 23:22:23 -0700

    Add: Do'kon cheki (int va float birga) [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/01-Do_kon_cheki__int_va_float_birga_.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/01-Do_kon_cheki__int_va_float_birga_.py
@@ -1,9 +1,4 @@
-maxsulot1 = "Non"
-narx1 = 4500 
-maxsulot2 = "Sut"
-narx2 = 12.5
-
 print("=== CHEK ===")
-print(f"{maxsulot1} - {narx1}")
-print(f"{maxsulot2} - {narx2}")
-print("Raxmat!")
+print("Non - 4500")
+print("Sut - 12.5")
+print("Rahmat!")
