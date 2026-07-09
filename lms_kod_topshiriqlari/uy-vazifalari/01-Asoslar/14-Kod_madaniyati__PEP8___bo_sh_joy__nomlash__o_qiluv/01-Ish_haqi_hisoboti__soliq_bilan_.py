cd7835919456a8931795a2f32456a8a7f67e3fcd
e58569539-wq <e58569539@gmail.com>
2026-07-09 13:25:57 -0700

    Add: Ish haqi hisoboti (soliq bilan) [e58569539-wq]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/14-Kod_madaniyati__PEP8___bo_sh_joy__nomlash__o_qiluv/01-Ish_haqi_hisoboti__soliq_bilan_.py
@@ -0,0 +1,13 @@
+soat = int(input())
+stavka = int(input())
+foiz = int(input())
+ 
+yalpi = soat * stavka 
+
+soliq = (yalpi * foiz) // 100
+
+sof = yalpi - soliq 
+
+print(yalpi)
+print(soliq)
+print(sof)
