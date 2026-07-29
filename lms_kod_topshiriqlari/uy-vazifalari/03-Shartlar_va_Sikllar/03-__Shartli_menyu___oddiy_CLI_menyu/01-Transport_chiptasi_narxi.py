da57431cb3969a5b0efef49e31b67f2b87a786af
e58569539-wq <e58569539@gmail.com>
2026-07-29 11:33:58 -0700

    Add: Transport chiptasi narxi [e58569539-wq]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Transport_chiptasi_narxi.py
@@ -0,0 +1,23 @@
+transport = input()
+toifa = input()
+
+if transport == '1':
+    narx = 1700
+elif transport == '2':
+    narx = 1700
+elif transport == '3':
+    narx = 4000
+else:
+    narx = None
+    
+if narx is None:
+    print('Notogri transport')
+else:
+    if toifa == '1':
+        print(narx)
+    elif toifa == '2':
+        print(narx // 2)
+    elif toifa == '3':
+        print(0)
+    else:
+        print('Notogri toifa')
