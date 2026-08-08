b586392d1e17a8f0d38daba69e1e6e9da36f18da
e58569539-wq <e58569539@gmail.com>
2026-08-08 00:09:46 -0700

    Add: Tub sonlar sanog'i [e58569539-wq]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Tub_sonlar_sanog_i.py
@@ -0,0 +1,21 @@
+tub_sonlar_soni = 0
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    if n < 2:
+        continue
+        
+    tub = True
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            tub = False
+            break
+        i += 1
+        
+    if tub:
+        tub_sonlar_soni += 1
+        
+print(tub_sonlar_soni)
