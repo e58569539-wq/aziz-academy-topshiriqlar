4404bd4ba5acd3f34456750935852e885d3a0ec9
e58569539-wq <e58569539@gmail.com>
2026-08-30 10:15:52 -0700

    Add: Musbat va manfiylar soni [e58569539-wq]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-Musbat_va_manfiylar_soni.py
@@ -0,0 +1,12 @@
+n = int(input())
+musbat = 0
+manfiy = 0
+
+for _ in range(n):
+    x = int(input())
+    if x > 0:
+        musbat += 1
+    elif x < 0:
+        manfiy += 1
+        
+print(musbat, manfiy)
