99c2aeac3647606c68d102f3db1885d1c8f1f433
e58569539-wq <e58569539@gmail.com>
2026-09-12 07:02:00 -0700

    Add: 2) Urinishlar sonini sanash [e58569539-wq]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/02-2__Urinishlar_sonini_sanash.py
@@ -0,0 +1,10 @@
+yashirin_son = 5
+urinishlar_soni = 0
+
+while True:
+    taxmin = int(input())
+    urinishlar_soni += 1
+    if taxmin == yashirin_son:
+        break
+    
+print(urinishlar_soni)
