23d4a89db819dcf120471e5ddc8c9a02869245d0
e58569539-wq <e58569539@gmail.com>
2026-09-14 09:01:49 -0700

    Add: Akkaunt holati bayroqlari [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
@@ -2,6 +2,6 @@ royxatdan_otgan = True
 email_tasdiqlangan = False
 bloklangan = False
 
-print(f"Ro'yxatdan o'tgan: {royxatdan_otgan}")
-print(f"Email tasdiqlangan: {email_tasdiqlangan}")
-print(f"Bloklangan: {bloklangan}")
+print("Ro'yxatdan o'tgan:", royxatdan_otgan)
+print("Email tasdiqlangan:", email_tasdiqlangan)
+print("Bloklangan:", bloklangan)
