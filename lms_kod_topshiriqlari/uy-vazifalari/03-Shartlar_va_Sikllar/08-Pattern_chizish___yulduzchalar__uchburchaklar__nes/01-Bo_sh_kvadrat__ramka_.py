65993668e429b0ab76afe6932a9ceeb24d7b50a3
e58569539-wq <e58569539@gmail.com>
2026-09-16 09:05:05 -0700

    Add: Bo'sh kvadrat (ramka) [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/08-Pattern_chizish___yulduzchalar__uchburchaklar__nes/01-Bo_sh_kvadrat__ramka_.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/08-Pattern_chizish___yulduzchalar__uchburchaklar__nes/01-Bo_sh_kvadrat__ramka_.py
@@ -1,9 +1,7 @@
 n = int(input())
 
-if n == 1:
-    print('*')
-else:
-    print('*' * n) 
-    for _ in range(n - 2):
+for i in range(n):
+    if i == 0 or i == n - 1:
+        print('*' * n)
+    else:
         print('*' + ' ' * (n - 2) + '*')
-    print('*' * n)
