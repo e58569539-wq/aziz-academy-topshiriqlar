5a132f4b2d91385976ba1a1e23ffe23f405ad8b4
e58569539-wq <e58569539@gmail.com>
2026-09-16 09:19:55 -0700

    Add: Oraliqdagi juft sonlar yig'indisi [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-Oraliqdagi_juft_sonlar_yig_indisi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-Oraliqdagi_juft_sonlar_yig_indisi.py
@@ -1,5 +1,10 @@
 a = int(input())
 b = int(input())
 
-yigindi = sum(i for i in range(a, b + 1) if i % 2 == 0)
+yigindi = 0
+
+for i in range(a, b + 1):
+    if i % 2 == 0:
+        yigindi += i
+        
 print(yigindi)
