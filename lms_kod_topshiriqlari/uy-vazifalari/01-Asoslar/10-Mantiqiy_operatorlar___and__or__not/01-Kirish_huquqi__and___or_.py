85db3792eff1fa7a3d7a8e3707ab2ade9bf0fa18
e58569539-wq <e58569539@gmail.com>
2026-09-15 22:28:59 -0700

    Add: Kirish huquqi (and / or) [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/10-Mantiqiy_operatorlar___and__or__not/01-Kirish_huquqi__and___or_.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/10-Mantiqiy_operatorlar___and__or__not/01-Kirish_huquqi__and___or_.py
@@ -1,5 +1,5 @@
 is_member = True
 has_ticket = False
 
-print(f"Ikkalasi ham (and): {is_member and has_ticket}")
-print(f"Kamida bittasi (or): {is_member or has_ticket}")
+print("Ikkalasi ham (and):", is_member and has_ticket)
+print("Kamida bittasi (or):", is_member or has_ticket)
