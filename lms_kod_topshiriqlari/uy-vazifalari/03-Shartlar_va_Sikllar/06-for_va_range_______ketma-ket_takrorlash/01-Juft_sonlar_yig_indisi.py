b1935591f5dee259f9008b9bae77052b075ed489
e58569539-wq <e58569539@gmail.com>
2026-09-19 07:55:50 -0700

    Add: Juft sonlar yig'indisi [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/06-for_va_range_______ketma-ket_takrorlash/01-Juft_sonlar_yig_indisi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/06-for_va_range_______ketma-ket_takrorlash/01-Juft_sonlar_yig_indisi.py
@@ -1,6 +1,9 @@
-n = int(input())
-s = 0
-for i in range(1, n + 1):
+N = int(input().strip())
+
+yigindi = 0
+
+for i in range(1, N + 1):
     if i % 2 == 0:
-        s += i
-print(s)
+        yigindi += i
+        
+print(yigindi)
