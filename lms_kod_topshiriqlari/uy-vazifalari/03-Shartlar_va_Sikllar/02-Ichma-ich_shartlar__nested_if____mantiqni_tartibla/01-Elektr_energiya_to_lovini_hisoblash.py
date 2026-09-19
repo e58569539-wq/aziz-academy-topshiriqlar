af7f2c056f705152ab5f47348eca7912aef152f7
e58569539-wq <e58569539@gmail.com>
2026-09-19 08:03:16 -0700

    Add: Elektr energiya to'lovini hisoblash [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Elektr_energiya_to_lovini_hisoblash.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Elektr_energiya_to_lovini_hisoblash.py
@@ -1,13 +1,15 @@
-kwh = int(input())
+kWh = int(input().strip())
 
-if kwh < 0:
+if kWh < 0:
     print("Notogri qiymat")
 else:
-    if kwh <= 100:
-        total = kwh * 450
-    elif kwh <= 200:
-        total = (100 * 450) + ((kwh - 100) * 600)
+    tolov = 0
+    
+    if kWh <= 100:
+        tolov = kWh * 450
+    elif kWh <= 200:
+        tolov = (100 * 450) + ((kWh - 100) * 600)
     else:
-        total = (100 * 450) + (100 * 600) + ((kwh - 200) * 900)
+        tolov = (100 * 450) + (100 * 600) + ((kWh - 200) * 900)
         
-    print(total)
+print(tolov)
