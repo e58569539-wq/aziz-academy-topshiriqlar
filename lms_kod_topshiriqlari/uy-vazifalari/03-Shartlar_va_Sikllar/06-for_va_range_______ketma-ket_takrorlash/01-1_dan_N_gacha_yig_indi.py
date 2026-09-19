3c09346f2e5efe2b60b6fd080843635668850448
e58569539-wq <e58569539@gmail.com>
2026-09-19 08:05:02 -0700

    Add: 1 dan N gacha yig'indi [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/06-for_va_range_______ketma-ket_takrorlash/01-1_dan_N_gacha_yig_indi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/06-for_va_range_______ketma-ket_takrorlash/01-1_dan_N_gacha_yig_indi.py
@@ -1,5 +1,8 @@
-n = int(input())
-s = 0
-for i in range(1, n + 1):
-    s += i
-print(s)
+N = int(input().strip())
+
+yigindi = 0
+
+for i in range(1, N + 1):
+    yigindi += i
+    
+print(yigindi)
