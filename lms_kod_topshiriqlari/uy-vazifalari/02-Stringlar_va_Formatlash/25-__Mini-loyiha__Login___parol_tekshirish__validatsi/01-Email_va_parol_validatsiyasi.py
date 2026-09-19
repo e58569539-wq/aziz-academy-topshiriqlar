6080ae6b61577b446f2a3d33c8cb783b672cc983
e58569539-wq <e58569539@gmail.com>
2026-09-19 07:32:53 -0700

    Add: Email va parol validatsiyasi [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/25-__Mini-loyiha__Login___parol_tekshirish__validatsi/01-Email_va_parol_validatsiyasi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/25-__Mini-loyiha__Login___parol_tekshirish__validatsi/01-Email_va_parol_validatsiyasi.py
@@ -1,6 +1,6 @@
-email = input()
-password = input()
+email = input().strip()
+parol = input().strip()
 
-is_valid_email = ("@" in email) and ("." in email) and (email == email.lower())
+shart = ('@' in email) and ('.' in email) and (8 <= len(parol) <= 16) and (email == email.lower())
 
-print(is_valid_email and is_valid_email)
+print(shart)
