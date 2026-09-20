dff7b5e3d1fa9b25e3f584c3a1239509af68b64d
e58569539-wq <e58569539@gmail.com>
2026-09-20 05:10:30 -0700

    Add: Bankomat menyusi [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Bankomat_menyusi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Bankomat_menyusi.py
@@ -1,15 +1,15 @@
-action = int(input())
-balance = int(input())
-amount = int(input())
+amal = int(input())
+balans = int(input())
+summa = int(input())
 
-if action == 1:
-    print(balance)
-elif action == 2:
-    if amount <= balance:
-        print(balance - amount)
+if amal == 1:
+    print(balans)
+elif amal == 2:
+    if summa <= balans:
+        print(balans - summa)
     else:
         print("Mablag' yetarli emas")
-elif action == 3:
-    print(balance + amount)
+elif amal == 3:
+    print(balans + summa)
 else:
     print("Notogri amal")
