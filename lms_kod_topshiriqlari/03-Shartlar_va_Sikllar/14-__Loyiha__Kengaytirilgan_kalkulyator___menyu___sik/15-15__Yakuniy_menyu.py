d3f57435097b34b700c31be766d5640907381ac6
e58569539-wq <e58569539@gmail.com>
2026-09-19 23:46:28 -0700

    Add: 15) Yakuniy menyu [e58569539-wq]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/15-15__Yakuniy_menyu.py
@@ -0,0 +1,21 @@
+while True:
+    line = input()
+    if line.strip() == "0":
+        print("Exit")
+        break
+    parts = line.split()
+    a = int(parts[0])
+    b = int(parts[1])
+    tanlov = int(input())
+    if tanlov == 1:
+        print(a + b)
+    elif tanlov == 2:
+        print(a - b)
+    elif tanlov == 3:
+        print(a * b)
+    elif tanlov == 4:
+        print(a / b)
+    elif tanlov == 5:
+        print(a % b)
+    elif tanlov == 6:
+        print(a ** b)
