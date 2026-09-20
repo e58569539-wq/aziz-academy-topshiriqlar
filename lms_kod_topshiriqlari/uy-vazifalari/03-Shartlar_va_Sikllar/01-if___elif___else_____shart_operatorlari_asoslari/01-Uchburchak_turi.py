587996d44b76b069442a9826983f4bc5920681ed
e58569539-wq <e58569539@gmail.com>
2026-09-19 21:46:59 -0700

    Add: Uchburchak turi [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Uchburchak_turi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Uchburchak_turi.py
@@ -3,11 +3,12 @@ b = int(input())
 c = int(input())
 
 if a > 0 and b > 0 and c > 0 and (a + b > c) and (a + c > b) and (b + c > a):
-    if a == b and b == c:
+    if a == b == c:
         print("teng tomonli")
-    elif a == b or a == c or b == c:
+    elif a == b or b == c or a == c:
         print("teng yonli")
     else:
         print("turli tomonli")
 else:
     print("notogri")
+        
