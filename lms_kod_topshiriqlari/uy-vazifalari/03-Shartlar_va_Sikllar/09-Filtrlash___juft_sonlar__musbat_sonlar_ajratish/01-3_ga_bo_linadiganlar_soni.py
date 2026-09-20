7cae4d7c8eaa2e5847986ca3dc22cce794cdb3c2
e58569539-wq <e58569539@gmail.com>
2026-09-20 09:53:10 -0700

    Add: 3 ga bo'linadiganlar soni [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-3_ga_bo_linadiganlar_soni.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-3_ga_bo_linadiganlar_soni.py
@@ -1,3 +1,9 @@
-import sys
-data = list(map(int, sys.stdin.read().split()))
-print(sum(1 for x in data[1:] if x % 3 == 0))
+n = int(input())
+natija = 0
+
+for _ in range(n):
+    x = int(input())
+    if x % 3 == 0:
+        natija += 1
+        
+print(natija)
