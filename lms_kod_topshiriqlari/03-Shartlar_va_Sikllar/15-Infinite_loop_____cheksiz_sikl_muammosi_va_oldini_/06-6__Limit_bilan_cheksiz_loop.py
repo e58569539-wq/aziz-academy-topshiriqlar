ef758dc97715ae2de1f5579bad824475b1726309
e58569539-wq <e58569539@gmail.com>
2026-09-20 00:52:40 -0700

    Add: 6) Limit bilan cheksiz loop [e58569539-wq]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/15-Infinite_loop_____cheksiz_sikl_muammosi_va_oldini_/06-6__Limit_bilan_cheksiz_loop.py
@@ -0,0 +1,11 @@
+# n = int(input())
+# while True.
+# Sanagich ishlating.
+# n martadan keyin break qiling.
+n = int(input())
+i = 0
+while True:
+    i += 1
+    print(i)
+    if i == n:
+        break
