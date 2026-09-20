4c62d3a1a4948483a0d1917dd8f893b6f533ae49
e58569539-wq <e58569539@gmail.com>
2026-09-19 23:06:24 -0700

    Add: 2) Ayirish menyusi [e58569539-wq]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/02-2__Ayirish_menyusi.py
@@ -0,0 +1,12 @@
+parts = input().split()
+a = int(parts[0])
+b = int(parts[1])
+tanlov = int(input())
+if tanlov == 1:
+    print(a + b)
+elif tanlov == 2:
+    print(a - b)
+elif tanlov == 3:
+    print(a * b)
+elif tanlov == 4:
+    print(a / b)
