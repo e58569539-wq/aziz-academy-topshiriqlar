a6d29c458da45f5e94505d4d04d7963867479e1d
e58569539-wq <e58569539@gmail.com>
2026-09-26 10:10:35 -0700

    Add: Sonning bo'luvchilari [e58569539-wq]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-Sonning_bo_luvchilari.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-Sonning_bo_luvchilari.py
@@ -1,8 +1,8 @@
 n = int(input())
+boluchilar = []
 
-boluvchilar = []
 for i in range(1, n + 1):
     if n % i == 0:
-        boluvchilar.append(str(i))
+        boluchilar.append(str(i))
         
-print(" ".join(boluvchilar))
+print(" ".join(boluchilar))
